a1e8ff49fea4e40e959ef83f83b3c76cd30348f3
IhorMyhaliak <ihor123dn@gmail.com>
2016-09-25 23:08:01 +0300

    initial commit

---
+++ M_Multithreading/M_Multithreading/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace M_Multithreading
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tbxUrls = new System.Windows.Forms.RichTextBox();
+            this.rtbStatus = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(34, 34);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "button1";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tbxUrls
+            // 
+            this.tbxUrls.Location = new System.Drawing.Point(257, 12);
+            this.tbxUrls.Name = "tbxUrls";
+            this.tbxUrls.Size = new System.Drawing.Size(491, 277);
+            this.tbxUrls.TabIndex = 1;
+            this.tbxUrls.Text = "";
+            // 
+            // rtbStatus
+            // 
+            this.rtbStatus.Location = new System.Drawing.Point(27, 109);
+            this.rtbStatus.Name = "rtbStatus";
+            this.rtbStatus.Size = new System.Drawing.Size(224, 349);
+            this.rtbStatus.TabIndex = 2;
+            this.rtbStatus.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 470);
+            this.Controls.Add(this.rtbStatus);
+            this.Controls.Add(this.tbxUrls);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.RichTextBox tbxUrls;
+        private System.Windows.Forms.RichTextBox rtbStatus;
+    }
+}
+
---
+++ M_Multithreading/M_Multithreading/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Configuration;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+namespace M_Multithreading
+{
+    public partial class Form1 : Form
+    {
+        private string[] urls;
+        private HttpClient client;
+        private CancellationTokenSource tokenSource;
+        private bool processing = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            urls = ConfigurationManager.AppSettings["urls"].Replace(" ", "").Split(new[] { "\r\n," }, StringSplitOptions.RemoveEmptyEntries);
+            client = new HttpClient();
+            tokenSource = new CancellationTokenSource();
+
+            tbxUrls.Text = string.Join(Environment.NewLine, urls);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (processing)
+            {
+                tokenSource.Cancel();
+                rtbStatus.Text += "Cancallation requested";
+                btnStart.Text = "Start";
+            }
+            else
+            {
+                processing = true;
+                btnStart.Text = "Stop";
+                foreach (var url in urls)
+                {
+                    //var t = new Task(() => GetData(url), tokenSource.Token);
+                    //t.Start();
+                    GetDataAsync(url, tokenSource.Token);
+                }
+            }
+        }
+
+        private void GetData(string url)
+        {
+            rtbStatus.Text += "\nProcessing " + url;
+
+            var response = client.GetAsync(url).Result;
+
+            rtbStatus.Text += "\nParsing response from " + url;
+
+            var result = response.Content.ReadAsStringAsync().Result;
+
+            ParseResult(result);
+        }
+
+
+        private async void GetDataAsync(string url, CancellationToken token)
+        {
+            try
+            {
+                rtbStatus.Text += "\nProcessing " + url;
+
+                var response = await client.GetAsync(url, token);
+
+                rtbStatus.Text += "\nParsing response from " + url;
+
+                var result = await response.Content.ReadAsStringAsync();
+
+                ParseResult(result);
+            }
+            catch (TaskCanceledException)
+            {
+                rtbStatus.Text += "\nTask was cancaleld";
+            }
+        }
+
+        private void ParseResult(string content)
+        {
+
+        }
+
+    }
+}
